8f0160b4be34e706e9a51110820e6d88961c09f5
Oleksii <oleksiy.salamatov@gmail.com>
2025-03-07 17:28:45 +0200

    Add migrations

---
+++ DeliveryOrderApp.Server/Migrations/20250307152452_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryOrderApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FromAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ToCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ToAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    OrderCollectedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
